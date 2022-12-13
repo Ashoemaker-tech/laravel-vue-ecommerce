a24411f7d60cd6a6989a52550e1afc7425398dd2
Andrew Shoemaker <79344687+Ashoemaker-tech@users.noreply.github.com>
2022-12-12 22:05:14 -0600

    admin UI

---
+++ admin/src/components/AppLayout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex min-h-full bg-gray-200">
+        <Sidebar />
+        <div class="flex-1">
+           <DashHeader /> 
+            <main class="p-6">
+                <div class="rounded p-4 bg-white">
+                    <router-view></router-view>
+                </div>
+            </main>
+        </div>
+    </div>
+  </template>
+  
+<script setup>
+    import Sidebar from './Sidebar.vue';
+    import DashHeader from './DashHeader.vue';
+</script>
---
+++ admin/src/components/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+ <!-- Sidebar -->
+  <div class="bg-indigo-700 w-64 text-gray-300 py-4 px-2">
+     <router-link to="dashboard" class="flex items-center transition-colors py-2 px-3 rounded hover:bg-black/30 mb-2">
+      <span class="mr-2">
+         <HomeIcon class="w-6"/>
+      </span>
+      <span >
+         Dashboard
+      </span>
+     </router-link> 
+      <router-link to="dashboard" class="flex items-center transition-colors py-2 px-3 rounded hover:bg-black/30 mb-2">
+      <span class="mr-2">
+         <ViewColumnsIcon class="w-6"/>
+      </span>
+      <span >
+         Products
+      </span>
+     </router-link> 
+      <router-link to="dashboard" class="flex items-center transition-colors py-2 px-3 rounded hover:bg-black/30 mb-2">
+      <span class="mr-2">
+         <UserIcon class="w-6"/>
+      </span>
+      <span >
+         Users
+      </span>
+     </router-link> 
+      <router-link to="dashboard" class="flex items-center transition-colors py-2 px-3 rounded hover:bg-black/30 mb-2">
+      <span class="mr-2">
+         <ChartBarIcon class="w-6"/>
+      </span>
+      <span >
+         Reports
+      </span>
+     </router-link> 
+
+  </div>     
+</template>
+
+<script setup>
+   import { HomeIcon, UserIcon, ViewColumnsIcon, ChartBarIcon } from '@heroicons/vue/20/solid';
+</script>
